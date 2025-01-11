bec07edceab1ff38fccc3d59e78564f073b92a46
davidran1 <157808421+davidran1@users.noreply.github.com>
2025-01-11 17:13:29 +0200

    team members

--- server/index.js
+++ server/index.js
@@ -1,25 +1,36 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-import costRouter from './routes/cost.route.js';
-import userRouter from './routes/user.route.js';
-import { connectDB } from './utils/db.js';
+import costRouter from "./routes/cost.route.js";
+import userRouter from "./routes/user.route.js";
+import { connectDB } from "./utils/db.js";
 
 //body paresr
-app.use(express.json({limit:"50mb"}));
+app.use(express.json({ limit: "50mb" }));
 
 //cookie parser
 //app.use(cookieParser());
 
-app.use('/api', costRouter , userRouter);
-const PORT = process.env.PORT||3000;
+app.use("/api", costRouter, userRouter);
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   connectDB();
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-app.get('/api/about', (req, res) => { 
-    res.send('Hello Project'); });
-
-
+app.get("/api/about", (req, res) => {
+  res.status(200).json({
+    success: true,
+    team_members: {
+      member1: {
+        first_name: "david",
+        last_name: "azran",
+      },
+      member2: {
+        first_name: "ofir",
+        last_name: "harar",
+      },
+    },
+  });
+});
